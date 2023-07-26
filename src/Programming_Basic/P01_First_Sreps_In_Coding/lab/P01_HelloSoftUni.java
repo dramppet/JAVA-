57aaa4169f4ee46f51db856a04d78effaeb918f0
dramp <dramppet@abv.bg>
2023-07-26 11:07:10 +0300

    new task

--- src/Programming_Basic/P01_First_Sreps_In_Coding/lab/P01_HelloSoftUni.java
+++ src/Programming_Basic/P01_First_Sreps_In_Coding/lab/P01_HelloSoftUni.java
@@ -1,4 +1,4 @@
-package Programming_basic.P01_First_Sreps_In_Coding.lab;
+package Programming_Basic.P01_First_Sreps_In_Coding.lab;
 
 public class P01_HelloSoftUni {
     public static void main(String[] args) {
