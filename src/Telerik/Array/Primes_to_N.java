f40bb200f625113a578c9e7b3b10b54bbb2edf7b
dramp <dramppet@abv.bg>
2023-10-11 10:20:09 +0300

    update task

--- src/Telerik/Array/Primes_to_N.java
+++ src/Telerik/Array/Primes_to_N.java
@@ -8,23 +8,16 @@ public class Primes_to_N {
 
         int num = sc.nextInt();
 
-        System.out.print(1 + " ");
-
         for(int i = 1; i <= num; i++){
 
            if (isPrime(i)){
                System.out.print( i  + " ");
            }
         }
-
-
     }
     public static boolean isPrime(int num){
 
-        if (num < 1){
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(num); i++){
+        for (int i = 2; i <= num / 2; i++){
             if (num % i == 0){
                 return false;
             }
